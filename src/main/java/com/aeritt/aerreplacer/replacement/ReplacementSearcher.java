286b8cf675f95b2202ee4f784a1271e344caf9be
whereareiam <mmm.g032@gmail.com>
2024-02-04 14:30:02 +0100

    fix

--- src/main/java/com/aeritt/aerreplacer/replacement/ReplacementSearcher.java
+++ src/main/java/com/aeritt/aerreplacer/replacement/ReplacementSearcher.java
@@ -11,42 +11,42 @@ import java.util.stream.Stream;
 
 @Singleton
 public class ReplacementSearcher {
-    public List<Path> search(List<String> placeholders, Path path, List<String> customPaths) {
-        List<Path> result = new ArrayList<>();
+	public List<Path> search(List<String> placeholders, Path path, List<String> customPaths) {
+		List<Path> result = new ArrayList<>();
 
-        for (String customPath : customPaths) {
-            result.addAll(search(placeholders, path.resolve(customPath)));
-        }
+		for (String customPath : customPaths) {
+			result.addAll(search(placeholders, path.resolve(customPath)));
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public List<Path> search(List<String> placeholders, Path path) {
-        List<Path> result = new ArrayList<>();
+	public List<Path> search(List<String> placeholders, Path path) {
+		List<Path> result = new ArrayList<>();
 
-        if (!Files.exists(path)) {
-            return result;
-        }
+		if (!Files.exists(path)) {
+			return result;
+		}
 
-        try (Stream<Path> paths = Files.walk(path)) {
-            paths.filter(Files::isRegularFile)
-                    .forEach(file -> {
-                        try {
-                            String content = new String(Files.readAllBytes(file));
-                            for (String placeholder : placeholders) {
-                                if (content.contains(placeholder)) {
-                                    result.add(file);
-                                    break;
-                                }
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		try (Stream<Path> paths = Files.walk(path)) {
+			paths.filter(Files::isRegularFile)
+					.forEach(file -> {
+						try {
+							String content = new String(Files.readAllBytes(file));
+							for (String placeholder : placeholders) {
+								if (content.contains(placeholder)) {
+									result.add(file);
+									break;
+								}
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
